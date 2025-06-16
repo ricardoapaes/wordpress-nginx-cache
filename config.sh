d71f8f82f32d787f927df7fe583392bf38bbf8af
Ricardo Paes <ricardo@likesistemas.com.br>
2025-06-16 16:54:21 -0300

    Fazendo build no docker-compose

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "Changing host to ${WORDPRESS_PROTOCOL}://${WORDPRESS_HOST}:${WORDPRESS_PORT}"
+echo "Changing host to ${WORDPRESS_PROTOCOL}://${WORDPRESS_HOST}:${WORDPRESS_PORT} (Domain: ${WORDPRESS_DOMAIN})"
 dockerize -template /etc/nginx/conf.d/default.tpl:/etc/nginx/conf.d/default.conf
-
 dockerize -wait tcp://${WORDPRESS_HOST}:${WORDPRESS_PORT} -timeout 60s
