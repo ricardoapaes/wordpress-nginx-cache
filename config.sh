168bae0ea2e2782d00c381666557e70bd75d20e3
Ricardo Paes <ricardo@rpaes.com.br>
2025-06-16 15:34:51 -0300

    Corrigindo script e definindo variáveis ambiente

--- config.sh
+++ config.sh
@@ -2,4 +2,5 @@
 
 echo "Changing host to ${WORDPRESS_PROTOCOL}://${WORDPRESS_HOST}:${WORDPRESS_PORT}"
 dockerize -template /etc/nginx/conf.d/default.tpl:/etc/nginx/conf.d/default.conf
-    -wait tcp://$WORDPRESS_HOST:${WORDPRESS_PORT} -timeout 60s
+
+dockerize -wait tcp://${WORDPRESS_HOST}:${WORDPRESS_PORT} -timeout 60s
